8bf1dea3e4e35afdc84d01bdf52bbc4d52adde6f
tahaasefnia <ta.asefnia0@gmail.com>
2026-09-03 18:05:24 +0330

    CreateEntities

---
+++ DailyTask.API/Entities/Priority.cs
@@ -0,0 +1,9 @@
+namespace DailyTask.API.Entities;
+
+public enum Priority
+{
+    Low = 1,
+    Normal = 2,
+    High = 3,
+    Critical = 4
+}
---
+++ DailyTask.API/Entities/Todo.cs
@@ -0,0 +1,10 @@
+namespace DailyTask.API.Entities;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Priority Priority{ get; set; }
+    public bool IsCompleted { get; set; }
+}
