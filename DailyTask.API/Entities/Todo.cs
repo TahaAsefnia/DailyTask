2b5d90ad5a1ec85580814a0493ad80b96e58bf96
tahaasefnia <ta.asefnia0@gmail.com>
2026-09-03 18:27:38 +0330

    fixingTodo

--- DailyTask.API/Entities/Todo.cs
+++ DailyTask.API/Entities/Todo.cs
@@ -1,10 +1,10 @@
 namespace DailyTask.API.Entities;
 
-public class Todo
+public class Task
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public Priority Priority{ get; set; }
-    public bool IsCompleted { get; set; }
+    public Priority Priority{ get; set; } =  Priority.Normal;
+    public bool IsCompleted { get; set; } = false;
 }
