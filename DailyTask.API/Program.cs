f5765c5f02616006d1677efc4e06c2c2eaddccc3
tahaasefnia <ta.asefnia0@gmail.com>
2026-09-04 22:30:31 +0330

    done

--- DailyTask.API/Program.cs
+++ DailyTask.API/Program.cs
@@ -8,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<DailyTaskDbContext>(options => options.UseSqlite("Data Source=DailyTaskDB.db"));
-
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(p => p
+        .WithOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+        .AllowAnyHeader()
+        .AllowAnyMethod()));
 
 var app = builder.Build();
 
@@ -28,7 +32,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 */
-
+app.UseCors();
 
 app.MapTaskEndpoints();
 app.Run();
